661496bf6117119c63081d95a62bd25de02db29b
Gustavo Ferreira Santos <gusf723@gmail.com>
2024-10-18 12:08:40 -0300

    adicionando cucumber no projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://thinking-tester-contact-list.herokuapp.com/',
     setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber());
     },
+    specPattern: 'cypress/e2e/**/*.feature',
     reporter: 'mochawesome', // Define o reporter como mochawesome
     reporterOptions: {
       reportDir: 'cypress/reports/mocha', // Diretório para os relatórios
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-cucumber-preprocessor/steps'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
